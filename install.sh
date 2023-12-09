ba3516f477012ace598f4dd84625a2fc6bcbaa14
Aarron Lee <aclee91@gmail.com>
2023-12-09 18:34:52 -0500

    cleanup old files if they exist

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 sudo systemctl disable steam-powerbuttond
 sudo systemctl stop steam-powerbuttond
 
+sudo rm /usr/local/bin/steam-powerbuttond
+sudo rm /etc/systemd/system/steam-powerbuttond.service 
+
 chmod +x ./steam-powerbuttond
 sudo cp ./steam-powerbuttond /usr/local/bin
 sudo cp ./steam-powerbuttond.service /etc/systemd/system
