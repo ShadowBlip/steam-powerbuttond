6c89b0e94661d4e91ab8e212c27a9f8b047f8ccf
Aarron Lee <aarron-lee@users.noreply.github.com>
2024-05-01 16:01:44 -0400

    update bazzite installer

--- bazzite_install.sh
+++ bazzite_install.sh
@@ -5,38 +5,16 @@ cd /tmp
 git clone https://github.com/aarron-lee/steam-powerbuttond.git && cd steam-powerbuttond
 
 chmod +x ./steam-powerbuttond
-mkdir -p $HOME/.local/bin
 
-rm $HOME/.local/bin/steam-powerbuttond
+sudo cp ./steam-powerbuttond /usr/local/bin/
 
-cp ./steam-powerbuttond $HOME/.local/bin
+sudo chcon -u system_u -r object_r --type=bin_t /usr/local/bin/steam-powerbuttond
 
-# handle for SE linux
-sudo chcon -u system_u -r object_r --type=bin_t /var/home/$USER/.local/bin/steam-powerbuttond
+sudo systemctl disable --now steam-powerbuttond
 
-sudo systemctl disable --now steam-powerbuttond-bazzite
-
-# cannot directly cat into /etc/systemd/system/ (probably due to se linux)
-cat << EOF >> "./steam-powerbuttond-bazzite.service"
-[Unit]
-Description=steam powerbuttond bazzite local service
-After=graphical-session.target
-
-[Service]
-Type=simple
-Nice = -15
-Restart=always
-RestartSec=5
-WorkingDirectory=/var/home/$USER/.local/bin/
-ExecStart=/var/home/$USER/.local/bin/steam-powerbuttond
-
-[Install]
-WantedBy=default.target
-EOF
-
-sudo mv ./steam-powerbuttond-bazzite.service /etc/systemd/system/
+sudo cp ./steam-powerbuttond.service /etc/systemd/system/
 
 sudo systemctl daemon-reload
-sudo systemctl enable --now steam-powerbuttond-bazzite
+sudo systemctl enable --now steam-powerbuttond
 
 echo "Install Complete"
